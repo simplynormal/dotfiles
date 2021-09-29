be72185be33937c595a69ac2e8f8e2e46d6ad117
Hoang NGUYEN <baauco@gmail.com>
2021-09-29 21:25:22 +0700

    Now pulling from debian 11

--- packages/debian/dotnet.sh
+++ packages/debian/dotnet.sh
@@ -4,7 +4,7 @@ source ../common.sh
 
 function main() {
   createtmp
-  wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+  wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
   sudo dpkg -i packages-microsoft-prod.deb
 
   sudo apt update
